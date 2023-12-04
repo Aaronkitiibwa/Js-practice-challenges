add4eb6ccffb4dbacb3ad904d19bd79a1054296d
Nakyenalire Aaron <58532030+Aaronkitiibwa@users.noreply.github.com>
2023-12-04 04:35:43 -0800

    Func prompts a user to enter their year of birth
    Function prompts user to enter their year of birth. It returns minor if one is below 18 years, youth if the person is between 18 and 36 and elder is the person is above 36 years.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function myAge(){
+console.log("please enter your age" )
+}
+myAge()
+
+n=18;
+function check(){
+    return "you are a child"
+}
+console.log(check())
+
+
+if(age < 18){
+    return "you are a child"
+}
+else if(age=>18)
+
